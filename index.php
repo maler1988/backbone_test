05d71b43f3934f78c088585f6e240e3b20e4f2fa
Andreev A.A <maler1988@yandex.ru>
2015-04-17 12:40:05 +0300

    Учимся писать Backbone.js приложения

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ru-RU">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Учимся работать с backbone.js</title>
+	
+	<link href="/css/style.js" type="text/css" rel="stylesheet" />
+	
+	
+	<script src="http://yastatic.net/jquery/1.10.1/jquery.min.js"></script>
+	<script src="http://yastatic.net/jquery-ui/1.10.3/jquery-ui.min.js"></script>
+	<script src="/js/backbone-min.js"></script>
+	<script src="/js/underscore-min.js"></script>
+	<script src="/js/script.js"></script>
+</head>
+<body>
+<img alt="Backbone.js" src="/images/backbone.png" >
+<p>Работа с backbone.js по урокам сайта <a target="_blank" href="http://loftblog.ru/2013/01/08/razrabotka-veb-prilozheniya-na-backbone-js-chast-pervaya-znakomstvo-s-instrumentami/" >LiftBlog</a></p>
+</body>
+</html>
