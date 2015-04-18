f37cbe8f787f94e19085566f3e97f408c9bef7c2
Alexandr <maler1988@yandex.ru>
2015-04-18 23:01:14 +0400

    Backbone.js 12 — пространство имен (пока с ошибкой)

--- js/script.js
+++ js/script.js
@@ -1,174 +1,105 @@
-//Очень похоже на ООП php 
-/*
+//Пространства имён
+(function() {
 
-//конструктор объекта Person
-var Person = function(config) {
-  this.name = config.name;
-  this.age = config.age;
-  this.job = config.job;
-};
+	window.App = { //Глобальный объект App
+		Models: {},
+		Views: {},
+		Collections: {}
 
-//Статичный метод walk, добавлен в прототип "класса" Person
-Person.prototype.walk = function() {
-	return this.name + " is walking";
-}
+	}; 
+ 
+	//хэлпер шаблона (вынесен как глобальная функция, хотя это не очень правильно)
+	window.template = function(id) {
+		return _.template( $('#' + id).html() );
+	};
 
-var nick = new Person({ name: 'Nick', age: 24, job: 'Front-end developer' });
-
-console.log(nick);
-console.log(nick.walk());
-
-*/
-
-
-//Теперь то же самое но на Backbone
-
-
-//Хэлпер – вспомогательная функция, автоматизирующая часто повторяющиеся действия.
-//Хелпер шаблона представления
-var templateHelper = function(id) {
-	return _.template( $('#' + id).html() );
-};
-
-
-
-
-
-
-
-
-
-
-//Класс модели Person
-var Person = Backbone.Model.extend({
-	defaults: {
-			name: 'Name',
-			age: 0,
-			job: 'Job'
-		},
-
-	//Валидация входных параметров
-	validate: function(attrs, options) {
-
-			console.log(attrs);
-
-			if ( attrs.age <= 0 ) {
-				return 'Возраст должен быть положительным!';
+	//Модель человека
+	App.Models.Person = Backbone.Model.extend({
+		defaults: {
+				name: 'Name',
+				age: 1,
+				job: 'Job'
 			}
-
-			if (!attrs.name) {
-						return 'Чувак, ты же не думаешь, что у персоны не может быть имени?';
-			}
-	 
+	});
+ 
+ 
+	//Список людей
+	App.Collections.People = Backbone.Collection.extend({
+		model: App.Models.Person
+	});
+ 
+ 
+	//Вид списка людей
+	App.Views.People = Backbone.View.extend({
+		tagName: 'ul',
+ 
+		initialize: function() {
 		},
-
-	walk: function(){
-		return this.get("name") + " is walking.";
-	}
-});
-
-
-//Класс представления  PersonView
-var PersonView = Backbone.View.extend({	
-	//Привязываем this.el к конкретному элементу DOM структуры
-	tagName:"li",
-	template: templateHelper("employer"),
-
-	//При создании вида всегда срабатывает функйия initialize
-	initialize: function() {
-		this.render();
-	},
-
-	//Отрисовка вида
-	render: function() {
-		this.$el.html( this.template( this.model.toJSON() ) );
-
-		return this;
-	}
-
-});
-
-
-
-//Создание экземпляров моделей с представлениями
-
-var person = new Person();
-person.set({name:"Александр", age:26, job:"Программист"});
-var personView = new PersonView({model:person});
-
-var person2 = new Person();
-person2.set({name:"Лиза", age:23, job:"Верстальщик"});
-var personView2 = new PersonView({model:person2});
-
-
-
-
-
-//Прослушиваем ошибку
-person.on('invalid', function(model, error){
-   alert("Invalid: " + error);
-});
-
-person2.on('invalid', function(model, error){
-   alert("Invalid: " + error);
-});
-
-
-
-//Класс коллкеции PeopleCollection
-
-var PeopleCollection = Backbone.Collection.extend({
-	model: Person
-});
-
-
-//Представление коллекции
-var PeopleView = Backbone.View.extend({
-	tagName: 'ul',
-
-	initialize: function(){
-
-	},
-
-	//Рендер коллекции
-	render: function() {
+ 
+		render: function() {
 			this.collection.each(function(person) {
-				var personView = new PersonView({model: person});
+				var personView = new App.Views.Person({model: person});
+ 
 				this.$el.append(personView.render().el);
 			}, this);
-	 
+ 
 			return this;
 		}
-});
-
-
-var peopleCollection = new PeopleCollection();
-
-//Добавляем ранее созданные модели в коллекцию
-peopleCollection.add(person);
-peopleCollection.add(person2);
-
-
-//Добавляем модели в коллекцию в виде массива
-var peopleCollection2 = new PeopleCollection([
-	{
-		name: 'Петр',
-		age: 20,
-		job: 'Таксист'
-	},
-	{
-		name: 'Олег',
-		age: 24,
-		job: 'Менеджер'
-	},
-	{
-		name: 'Анна',
-		age: 18,
-		job: 'Студентка'
-	}
  
-]);
+	});
+ 
+ 
+	//Вид одного человека
+	App.Views.Person = Backbone.View.extend({
+		tagName: 'li',
+ 
+		template:  template('personTemplate'),
+ 
+ 
+		initialize: function() {
+			this.render();
+		},
+ 
+		render: function() {
+			//замечательный шаблон
+			this.$el.html( this.template( this.model.toJSON() ) );
+ 
+			return this;
+		}
+	});
 
-var peopleView = new PeopleView({collection: peopleCollection2});
 
-$(document.body).append(peopleView.render().el);
+var peopleCollection = new App.Collections.People([
+		{
+			name: 'Петр',
+			age: 20,
+			job: 'Таксист'
+		},
+		{
+			name: 'Олег',
+			age: 24,
+			job: 'Менеджер'
+		},
+		{
+			name: 'Анна',
+			age: 18,
+			job: 'Студентка'
+		}
+	]);
+ 
+ 
+	var peopleView = new App.Views.People({collection: peopleCollection});
+ 
+	$(document.body).append(peopleView.render().el);
+ 
+	console.log(App.Models);
+
+
+
+
+
+
+
+}());
+
+
