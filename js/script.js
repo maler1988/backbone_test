c6ba642b7c0cf6fc133937180eb8eda39009f479
Andreev A.A <maler1988@yandex.ru>
2015-04-20 13:56:10 +0300

    коммит который я забыл залить

--- js/script.js
+++ js/script.js
@@ -15,5 +15,36 @@ Person.prototype.walk = function() {
 
 var nick = new Person({ name: 'Nick', age: 24, job: 'Front-end developer' });
 
-console.log(nick);
-console.log(nick.walk());
+//console.log(nick);
+//console.log(nick.walk());
+
+
+
+
+//Теперь то же самое в backbone
+
+var PersonBackbone = Backbone.Model.extend({
+	defaults: {
+		name: "UserName",
+		age: 0,
+		job: "User job"
+	},
+	
+	walk: function() {
+		return this.get('name') + ' is walking.';
+	}
+
+});
+
+//Не указали никаких данных в конструкторе, создался пользователь со значениями из default
+var bPerson = new PersonBackbone;
+
+//Устанавливаем атрибуты объекта
+bPerson.set({"name":"Sasha", "age":26, "job":"Ruformat programmer"});
+
+//Получаем значение атрибутов объекта
+var Job = bPerson.get("job");
+
+console.log(bPerson);
+console.log("Job is: "+Job);
+console.log(bPerson.walk());
